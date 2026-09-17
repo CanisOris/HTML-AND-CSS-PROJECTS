f8e4f31cd105f7a5038102cc03e1a4665748b3b0
M@ <166262410+CanisOris@users.noreply.github.com>
2026-09-16 20:42:17 -0700

    Interface change. Fixes #2

---
+++ Basic_HTML_and_CSS/box/Hello_World.js
@@ -0,0 +1,8 @@
+var greet = 'Interface, changes make it hard to teach with this style';
+console.log(greet);
+
+var greet2 = 'Welcome connection to the node';
+console.log(greet2);
+
+var sentence1 = 'Ladybugs can be female and male';
+console.log(sentence1);
